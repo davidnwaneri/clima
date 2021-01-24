873098b40eee10aec4c921105cb1c1e794c2cf56
davidn <davidn4333@gmail.com>
2021-01-24 02:47:59 +0100

    added description

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/services/weather.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    WeatherModel weatherModel = WeatherModel();
+    var weatherData = await weatherModel.getLocationWeather();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => LocationScreen(locationWeather: weatherData)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SpinKitFadingCircle(
+            color: Colors.blue,
+            size: 100.0,
+          ),
+          SizedBox(height: 20.0),
+          Text(
+            'Getting necessary data.',
+            style: TextStyle(fontSize: 10.0),
+          )
+        ],
+      ),
+    );
+  }
+}
